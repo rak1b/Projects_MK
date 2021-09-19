63fc29736a0d44406b21916ce421ddeb408b4fec
rak1b <rak13.dev@gmail.com>
2021-09-19 19:33:53 +0600

    'basic-automation-done'

--- #3-Auto_Post/NationbuilderAuto.py
+++ #3-Auto_Post/NationbuilderAuto.py
@@ -1,12 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+start_time = time.time()
+
 # email = 'milos.rayn@acelap.com'
-email = 'kadan.seaver@acelap.com'
+# email = 'kadan.seaver@acelap.com'
+# email = 'mansoor.avyay@acelap.com'
+email = 'brisen.montrey@acelap.com'
 passw = 'Rakibrk11'
 # url = 'https://www.keliiakina.com/users/event_pages/new?parent_id=150'
 # url = 'https://www.okgrassrootsproject.com/users/event_pages/new?parent_id=6'
-url = "https://www.thebullyproject.com/users/event_pages/new?parent_id=2572"
+# url = "https://www.thebullyproject.com/users/event_pages/new?parent_id=2572"
+# url = "https://www.europeanmovement.co.uk/users/event_pages/new?parent_id=69"
+url = "https://www.parti-socialiste.fr/users/event_pages/new?parent_id=3302"
+
 headline = 'headline'
 venue = 'venue'
 address1 = 'address1'
@@ -26,31 +36,52 @@ driver.maximize_window()
 driver.get(url)
 # driver.get("https://www.thebullyproject.com/users/event_pages/new?parent_id=2572")
 
-try:
-    driver.find_element_by_xpath('//input[@id="user_session_email"]').send_keys(email)
-    driver.find_element_by_xpath('//input[@id="user_session_password"]').send_keys(passw)
-    driver.find_element_by_xpath('//input[@name="commit"]').click()
+def CheckLogin():
+    try:
+        driver.find_element_by_xpath('//input[@id="user_session_email"]').send_keys(email)
+        driver.find_element_by_xpath('//input[@id="user_session_password"]').send_keys(passw)
+        driver.find_element_by_xpath('//input[@name="commit"]').click()
 
-    driver.get(url)
-except:
-    print('Already logged in!')
+    except:
+        print('Already logged in!')
 
 
 def EventPost(headline,venue,address1,address2,city,state,zip,description):
-    driver.implicitly_wait(10)
+    driver.get(url)
+    # driver.implicitly_wait(10)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//input[@name="commit"]')))
     _headline = driver.find_element_by_xpath('//input[@id="event_page_page_headline"]').send_keys(headline)
-    _capacity = driver.find_element_by_xpath('//input[@id="event_page_capacity_count"]').send_keys('10')
+    try:_capacity = driver.find_element_by_xpath('//input[@id="event_page_capacity_count"]').send_keys('10')
+    except:pass
     _venue = driver.find_element_by_xpath('//input[@id="event_page_venue_name"]').send_keys(venue)
     _address1 = driver.find_element_by_xpath('//input[@id="event_page_venue_address_address1"]').send_keys(address1)
     _address2 = driver.find_element_by_xpath('//input[@id="event_page_venue_address_address2"]').send_keys(address2)
     _city = driver.find_element_by_xpath('//input[@id="event_page_venue_address_city"]').send_keys(city)
-    _state = driver.find_element_by_xpath('//input[@id="event_page_venue_address_state"]').send_keys(state)
+    try:_state = driver.find_element_by_xpath('//input[@id="event_page_venue_address_state"]').send_keys(state)
+    except:pass
     _postal_code = driver.find_element_by_xpath('//input[@id="event_page_venue_address_zip"]').send_keys(zip)
     _description_tiny = driver.find_element_by_xpath('//iframe[@id="event_page_content_editable_ifr"]').send_keys(f" {description}")
     _submit = driver.find_element_by_xpath('//input[@name="commit"]').click()
+    # time.sleep(2)
+    WebDriverWait(driver, 15).until(EC.url_changes(driver.current_url))
+    return driver.current_url
 
 
-EventPost(headline,venue,address1,address2,city,state,zip,description)
-print('working')
-# driver.get("https://www.google.co.in")
-# milos.rayn@acelap.com
+
+CheckLogin()
+links = []
+for i in range(5):
+    event_url = EventPost(headline,venue,address1,address2,city,state,zip,description)
+    print(event_url)
+    links.append(event_url)
+
+with open('eventLinks.txt', 'w',encoding="utf-8") as file_name:
+    for item in links:
+        file_name.write(f"{item}\n")
+    file_name.close()
+
+
+
+print("Time Taken : %s seconds ---" % (time.time() - start_time))
+time.sleep(10)
+driver.quit()
